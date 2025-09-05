a43f8deda5ab05c91ad42fd2cdf5113e3a408b83
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-09-05 10:23:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ window.addEventListener('scroll', () => {
    
 
 })
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickble:true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //responsividade breakpoint
+
+  breakpoints: {
+    0:{
+        slidePerView: 1
+    },
+    620: {
+      slidePerView: 2
+    },
+    1024:{
+      slidePerView: 3
+    }
+  }
+
+});
